678be5c7239b8bd055922eaf379c09530d326419
scan3ls <scan3ls@gmail.com>
2021-03-17 13:59:29 -0500

    Generating floats

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -5,7 +5,7 @@ import asyncio
 from random import uniform
 
 
-async def async_generator() -> Generator[int, None, None]:
+async def async_generator() -> Generator[float, None, None]:
     """generator function """
     for _ in range(10):
         await asyncio.sleep(1)
