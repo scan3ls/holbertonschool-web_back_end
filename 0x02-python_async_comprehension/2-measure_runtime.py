e4fdde0dcde298d3fbf9230ae20212f3a7cf7a66
scan3ls <scan3ls@gmail.com>
2021-03-17 14:08:55 -0500

    revert change

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python3
 """ module docstring super """
-import time
+from time import time
 import asyncio
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
 async def measure_runtime() -> float:
     """ measure delta t """
-    t1 = time.time()
+    t1 = time()
     await asyncio.gather(
         async_comprehension(),
         async_comprehension(),
         async_comprehension(),
         async_comprehension()
     )
-    t2 = time.time()
+    t2 = time()
     return t2 - t1
