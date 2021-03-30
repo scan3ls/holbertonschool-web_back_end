e9c5d04f4c1c7ceafe5063a18937ec7f0aa88815
scan3ls <scan3ls@gmail.com>
2021-03-30 15:02:31 -0500

    Add file

---
+++ 0x06-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,45 @@
+"""
+Authentication Module
+"""
+from flask import request
+from typing import List, TypeVar
+
+class Auth:
+    """
+    Authentication aclass
+    """
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """
+        Define which routes don't need authentications
+        """
+
+        if path is None or excluded_paths is None:
+            return True
+
+        s1 = [char for char in path]
+        if s1[-1] != '/':
+            s1.append('/')
+            path = "".join(s1)
+        
+        if path in excluded_paths:
+            return False
+        
+        return True
+
+    def authorization_header(self, request=None) -> str:
+        """
+        """
+        if request is None:
+            return None
+
+        for header in request.headers:
+            key, value = header
+            if key == 'Authorization':
+                return value
+
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ """
+        return None
---
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,10 @@
+""" Basic authentication
+"""
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """ Basic authentication
+    """
+
+    pass
