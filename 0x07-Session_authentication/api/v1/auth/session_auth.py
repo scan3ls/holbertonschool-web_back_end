47ece3973ab42f8d617247d3cfd251e86bea5bd5
scan3ls <scan3ls@gmail.com>
2021-04-05 13:16:43 -0500

    Create session for user

--- 0x07-Session_authentication/api/v1/auth/session_auth.py
+++ 0x07-Session_authentication/api/v1/auth/session_auth.py
@@ -1,7 +1,21 @@
 #!/usr/bin/env python3
 """ Session authentication """
-from api.v1.auth.auth import Auth 
+from api.v1.auth.auth import Auth
+from uuid import uuid4
 
 
 class SessionAuth(Auth):
     """ Session authentication """
+
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """ creates a Session for a user """
+        if user_id is None:
+            return None
+        if isinstance(user_id, str) is False:
+            return None
+
+        sessionID = str(uuid4())
+        self.user_id_by_session_id[sessionID] = user_id
+        return sessionID
