af515137c59b94d1a978693a4a41b7027396f101
scan3ls <scan3ls@gmail.com>
2021-04-05 13:43:20 -0500

    Add check for cookies

--- 0x07-Session_authentication/api/v1/app.py
+++ 0x07-Session_authentication/api/v1/app.py
@@ -59,12 +59,16 @@ def before():
     exclueded_paths = [
         '/api/v1/status/',
         '/api/v1/unauthorized/',
-        '/api/v1/forbidden/'
+        '/api/v1/forbidden/',
+        '/api/v1/auth_session/login/'
     ]
     if not auth.require_auth(request.path, exclueded_paths):
         return
 
     if auth.authorization_header(request) is None:
+        abort(401)
+
+    if auth.session_cookie(request) is None:
         abort(401)
 
     if auth.current_user(request) is None:
