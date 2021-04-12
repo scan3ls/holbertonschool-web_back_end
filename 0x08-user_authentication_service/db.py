b16a339110029c92e65f1ccc24ba9a8467400534
scan3ls <scan3ls@gmail.com>
2021-04-12 13:05:40 -0500

    Remove TypeVar from typing

--- 0x08-user_authentication_service/db.py
+++ 0x08-user_authentication_service/db.py
@@ -27,7 +27,7 @@ class DB:
             self.__session = DBSession()
         return self.__session
 
-    def add_user(self, email: str, hashed_password: str) -> TypeVar('User'):
+    def add_user(self, email: str, hashed_password: str) -> User:
         """ add user to db """
         new_user = User(email=email, hashed_password=hashed_password)
         self._session.add(new_user)
