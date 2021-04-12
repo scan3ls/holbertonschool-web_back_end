d86f7c4283d688b4aedc2d7848ad138d5c5ecb0e
scan3ls <scan3ls@gmail.com>
2021-04-12 15:33:09 -0500

    Abort if session_id is None

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -57,6 +57,9 @@ def login() -> str:
 def logout():
     """ user logout """
     session_id = request.cookies.get('session_id')
+    if session_id is None:
+        return abort(403)
+
     user = AUTH.get_user_from_session_id(session_id)
 
     if user is None:
