fbe9b0b0334412676f239e0a3dcf9f1cb45a7ff4
scan3ls <scan3ls@gmail.com>
2021-04-12 14:24:27 -0500

    Add register user route

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -6,18 +6,32 @@ from os import getenv
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
-
+from auth import Auth
 
 app = Flask(__name__)
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
-
+AUTH = Auth()
 
 @app.route('/', methods=["GET"], strict_slashes=False)
 def basic() -> str:
     """ Basic home
     """
     return jsonify({"message": "Bienvenue"}), 200
+
+
+@app.route('/users', methods=["POST"], strict_slashes=False)
+def users() -> str:
+    """ register a user
+    """
+    email = request.form.get('email')
+    password = request.form.get('password')
+    try:
+        AUTH.register_user(email, password)
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
+
+    return jsonify({"email": email, "message": "user created"}), 200
 
 
 if __name__ == "__main__":
