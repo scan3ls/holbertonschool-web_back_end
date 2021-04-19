d5070e89c56b26bec119b2666951e72ab0703c8a
scan3ls <scan3ls@gmail.com>
2021-04-19 12:28:42 -0500

    Test client.org()

--- 0x09-Unittests_and_integration_tests/test_client.py
+++ 0x09-Unittests_and_integration_tests/test_client.py
@@ -1,1 +1,20 @@
 #!/usr/bin/env python3
+
+from client import GithubOrgClient
+from parameterized import parameterized
+import unittest
+from unittest.mock import patch, Mock
+from client import GithubOrgClient
+
+
+class TestGithubOrgClient(unittest.TestCase):
+    """ Test client file """
+
+    @parameterized.expand([
+        ('google'),
+        ('abc')
+    ])
+    @patch('client.get_json')
+    def test_org(self, org, foo):
+        GithubOrgClient(org).org()
+        foo.assert_called_once_with(f'https://api.github.com/orgs/{org}')
