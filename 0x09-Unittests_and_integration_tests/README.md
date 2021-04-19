59df46466d1c2d3efa2f3ed828b32f15f32de932
scan3ls <scan3ls@gmail.com>
2021-04-18 22:24:20 -0500

    Add file

---
+++ 0x09-Unittests_and_integration_tests/README.md
@@ -0,0 +1,8 @@
+# 0x09. Unittests_and_integration_tests
+
+## Introduction
+This repository is used for project based learning in Holberton.
+
+## Learning Objectives
+- The difference between unit and integration tests.
+- Common testing patterns such as mocking, parametrizations and fixtures
---
+++ 0x09-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+""" test module """
+from parameterized import parameterized
+from nose.tools import assert_equal
+import unittest
+from unittest.mock import patch, Mock
+from utils import access_nested_map, get_json
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    """ test suite for AccesNestedMap """
+
+    @parameterized.expand([
+        ({'a': 1}, ('a',), 1),
+        ({'a': {'b': 2}}, ('a',), {'b': 2}),
+        ({'a': {'b': 2}}, ('a', 'b'), 2)
+    ])
+    def test_access_nested_map(self, mapping, sequence, expected):
+        """ test access nested map """
+        assert_equal(access_nested_map(mapping, sequence), expected)
+
+    @parameterized.expand([
+        ({}, ('a',), KeyError),
+        ({'a': 1}, ('a', 'b'), KeyError)
+    ])
+    def test_access_nested_map_exception(self, mapping, sequence, expected):
+        """ test key error raise """
+        with self.assertRaises(Exception) as context:
+            access_nested_map(mapping, sequence)
+        assert_equal(type(context.exception), expected)
+
+
+class TestGetJson(unittest.TestCase):
+    """ test suite for GetJson """
+
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ('http://holberton.io', {'payload': False})
+    ])
+    def test_get_json(self, test_url, test_payload):
+        """ test get_json """
+        request = Mock()
+        request.json.return_value = test_payload
+        with patch('requests.get', return_value=request):
+            response = get_json(test_url)
+            request.json.assert_called_once()
+            assert_equal(response, test_payload)
+
+
+if __name__ == "__main__":
+    unittest.main()
