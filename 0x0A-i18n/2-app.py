65fb64ba6eae608a087154d932acfe0a9e23edf2
scan3ls <scan3ls@gmail.com>
2021-04-22 12:30:52 -0500

    Find best_match

--- 0x0A-i18n/2-app.py
+++ 0x0A-i18n/2-app.py
@@ -21,10 +21,12 @@ class Config():
 
 app.config.from_object(Config)
 
+
 @babel.localeselector
 def get_locale():
     """ get best language """
-    return request.accept_languages
+    return request.accept_languages.best_match(app.config[LANGUAGES])
+
 
 @app.route('/')
 def index():
