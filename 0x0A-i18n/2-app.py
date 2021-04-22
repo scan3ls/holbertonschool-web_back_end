ffbf5f7d8eb28cc303798090eca7acc6d68864dc
scan3ls <scan3ls@gmail.com>
2021-04-22 12:24:48 -0500

    Add babel.localeselector

---
+++ 0x0A-i18n/2-app.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+""" basic flask app """
+from flask import (
+    Flask,
+    render_template,
+    request
+)
+from flask_babel import Babel
+
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config():
+    """ language config for babel """
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+
+@babel.localeselector
+def get_locale():
+    """ get best language """
+    return request.accept_languages
+
+@app.route('/')
+def index():
+    """ simple route to index.html """
+    return render_template('2-index.html')
