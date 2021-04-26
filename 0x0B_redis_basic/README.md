9f5364b985bb0aa2490501dfde62933302dfda73
scan3ls <scan3ls@gmail.com>
2021-04-26 12:22:11 -0500

    Add file

---
+++ 0x0B_redis_basic/README.md
@@ -0,0 +1,8 @@
+# 0x0B_redis_basic
+
+## Introduction
+This repository is used for project based learning in Holberton.
+
+## Learning Objectives
+- Learn how to use redis for basic operations
+- Learn how to use redis as a simple cache
---
+++ 0x0B_redis_basic/exercise.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+""" redis exercises """
+from redis import Redis
+from typing import Union, Any, Callable
+
+
+class Cache():
+    """ caching class """
+
+    def __init__(self):
+        """ constructor """
+        self._redis = Redis()
+        self._redis.flushdb()
+
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        """ store data in redis db """
+        from uuid import uuid4
+
+        key = str(uuid4())
+
+        self._redis.append(key, data)
+        self._redis.save()
+
+        return key
+
+    def get(self, key: str, fn: Callable) -> Any:
+        """ return data in original form """
+        value = self._redis.get(key)
+        return fn(value) if fn is not None else value
+
+    def get_str(self, key: str) -> str:
+        try:
+            return self.get(key, str)
+        except ValueError as e:
+            print(e)
+
+    def get_int(self, key: str) -> int:
+        try:
+            return self.get(key, int)
+        except ValueError as e:
+            print(e)
