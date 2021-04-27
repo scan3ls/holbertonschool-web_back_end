82bf31a9ec1a592ec2efeb580b2a133f2808536b
scan3ls <scan3ls@gmail.com>
2021-04-27 01:03:16 -0500

    Remove get()

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -21,22 +21,3 @@ class Cache():
         self._redis.save()
 
         return key
-
-    def get(self, key: str, fn: Callable) -> Union[str, bytes, int, float]:
-        """ return data in original form """
-        value = self._redis.get(key)
-        return fn(value) if fn is not None else value
-
-    def get_str(self, key: str) -> str:
-        """ get w/ str function """
-        try:
-            return self.get(key, str)
-        except ValueError as e:
-            print(e)
-
-    def get_int(self, key: str) -> int:
-        """ get w/ int function """
-        try:
-            return self.get(key, int)
-        except ValueError as e:
-            print(e)
