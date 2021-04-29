a22f94c8a9eea83e7393f9d2921b82ecff430ce5
scan3ls <scan3ls@gmail.com>
2021-04-29 13:24:36 -0500

    Add file

---
+++ 0x0C-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,11 @@
+-- List Glam rock bands
+-- Order by lifespan
+SELECT
+    band_name AS band_name
+    ,(IFNULL(split, YEAR(CURDATE())) - formed) AS lifespan
+    FROM
+        metal_bands
+    WHERE
+        style LIKE '%Glam Rock%'
+    ORDER BY
+        lifespan DESC
