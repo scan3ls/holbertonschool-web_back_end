a9e54b102e3b37db178e9f2b758c37a968a41296
scan3ls <scan3ls@gmail.com>
2021-04-29 16:49:29 -0500

    Add file

---
+++ 0x0C-MySQL_Advanced/7-average_score.sql
@@ -0,0 +1,26 @@
+-- Create Stored procedure ComputeAverageScoreForUser
+-- Compute and update average score for users from corrections
+
+DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
+DELIMITER //
+
+CREATE
+    PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
+    BEGIN
+        DECLARE average INT DEFAULT 0;
+
+        SELECT
+            SUM(score) / COUNT(user_id)
+        INTO average
+        FROM corrections
+        WHERE
+            corrections.user_id = user_id;
+
+        UPDATE users
+        SET
+            average_score = average
+        WHERE
+            id = user_id;
+    END//
+
+DELIMITER ;
