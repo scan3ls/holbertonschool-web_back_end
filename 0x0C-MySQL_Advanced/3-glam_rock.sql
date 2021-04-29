337c9dca7db045b07834c18c9ef0a28c25ec427a
scan3ls <scan3ls@gmail.com>
2021-04-29 13:36:59 -0500

    Update

--- 0x0C-MySQL_Advanced/3-glam_rock.sql
+++ 0x0C-MySQL_Advanced/3-glam_rock.sql
@@ -2,8 +2,10 @@
 -- Order by lifespan
 SELECT
     band_name
-    ,IFNULL(split, 2020) - formed AS lifespan
+    ,IFNULL(split, YEAR(CURDATE())) - formed AS lifespan
     FROM
         metal_bands
     WHERE
         style LIKE '%Glam rock%'
+    ORDER by
+        lifespan
