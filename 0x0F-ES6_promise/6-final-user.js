9675109f028f3ab78c7e3da2476c7b3ac842508b
scan3ls <scan3ls@gmail.com>
2021-05-11 18:17:21 -0500

    Fix return Value

--- 0x0F-ES6_promise/6-final-user.js
+++ 0x0F-ES6_promise/6-final-user.js
@@ -1,13 +1,22 @@
-import signUpUser from "./4-user-promise.js"
-import uploadPhoto from "./5-photo-reject.js"
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
 function handleProfileSignup(firstName, lastName, fileName) {
+  const res1 = signUpUser(firstName, lastName);
+  const res2 = uploadPhoto(fileName);
 
-    const res1 = signUpUser(firstName, lastName).catch();
-    const res2 = uploadPhoto(fileName).catch();
-
-    return Promise.all([res1, res2])
-    .finally((values) => value).catch(() => {});
+  return Promise.allSettled([res1, res2]).then(
+    (thing) => {
+      for (const obj of thing) {
+        const value = obj.reason;
+        if (value !== undefined) {
+          delete obj.reason;
+          obj.value = value.toString();
+        }
+      }
+      return thing;
+    },
+  );
 }
 
 export default handleProfileSignup;
