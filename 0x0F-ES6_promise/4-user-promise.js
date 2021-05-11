40e32a9871ba172db85c32c606dbf259c8025490
scan3ls <scan3ls@gmail.com>
2021-05-11 17:28:36 -0500

    Fix eslint

--- 0x0F-ES6_promise/4-user-promise.js
+++ 0x0F-ES6_promise/4-user-promise.js
@@ -1,10 +1,10 @@
 function signUpUser(firstName, lastName) {
-    return Promise.resolve(
-        {
-            'firstName': firstName,
-            'lastName': lastName
-        }
-    );
+  return Promise.resolve(
+    {
+      firstName,
+      lastName,
+    },
+  );
 }
 
 export default signUpUser;
