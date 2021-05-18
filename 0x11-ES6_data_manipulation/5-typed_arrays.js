1e9168744b30996274fc2763bf130929db09fdab
scan3ls <scan3ls@gmail.com>
2021-05-18 08:58:56 -0500

    Fix length in position check

--- 0x11-ES6_data_manipulation/5-typed_arrays.js
+++ 0x11-ES6_data_manipulation/5-typed_arrays.js
@@ -2,7 +2,7 @@ export default function createInt8TypedArray(length, position, value) {
   const buffer = new ArrayBuffer(length);
   const view = new DataView(buffer);
 
-  if (position <= 0 >= 10) {
+  if (position <= 0 >= length) {
     throw Error('Position outside range');
   }
   view.setUint8(position, value);
