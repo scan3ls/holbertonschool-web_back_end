dae6ba77058f0b563b231c5782f2e3a53e96ef73
scan3ls <scan3ls@gmail.com>
2021-05-18 17:59:37 -0500

    Fix typo

--- 0x11-ES6_data_manipulation/8-clean_set.js
+++ 0x11-ES6_data_manipulation/8-clean_set.js
@@ -1,7 +1,7 @@
 export default function cleanSet(set, startString) {
   const result = [];
 
-  if (set instanceof Set === False) return '';
+  if (set instanceof Set === false) return '';
   if (typeof startString !== 'string') return '';
   if (startString.length <= 0) return '';
 
