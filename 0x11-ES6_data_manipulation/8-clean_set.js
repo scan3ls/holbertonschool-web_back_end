b31edae9132a2dba388f1cf59cb01a31e28c09e6
scan3ls <scan3ls@gmail.com>
2021-05-18 17:59:18 -0500

    Check if set is a set

--- 0x11-ES6_data_manipulation/8-clean_set.js
+++ 0x11-ES6_data_manipulation/8-clean_set.js
@@ -1,6 +1,7 @@
 export default function cleanSet(set, startString) {
   const result = [];
 
+  if (set instanceof Set === False) return '';
   if (typeof startString !== 'string') return '';
   if (startString.length <= 0) return '';
 
