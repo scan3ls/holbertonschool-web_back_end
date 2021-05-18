39e318c391c1b8f84941a9412329e93b6e7cd1cc
scan3ls <scan3ls@gmail.com>
2021-05-18 08:47:11 -0500

    Add type check

--- 0x11-ES6_data_manipulation/8-clean_set.js
+++ 0x11-ES6_data_manipulation/8-clean_set.js
@@ -4,6 +4,7 @@ export default function cleanSet(set, startString) {
   if (startString.length <= 0) return '';
 
   [...set].forEach((value) => {
+    if (typeof value !== 'string') return;
     if (value.startsWith(startString)) {
       result.push(value.substring(startString.length));
     }
