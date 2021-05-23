86b49e450e79cad71a0eb47a7a83b1cd8164649b
scan3ls <scan3ls@gmail.com>
2021-05-22 20:55:21 -0500

    Add file

---
+++ 0x12-Node_JS_basic/5-http.js
@@ -0,0 +1,51 @@
+const http = require('http');
+const readFile = require('./3-read_file_async');
+// const { runInNewContext } = require('vm');
+
+const host = '127.0.0.1';
+const port = 1245;
+
+const index = (res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello Holberton School!');
+};
+
+const students = (res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  const promise = readFile('database.csv');
+
+  promise
+    .then((data) => {
+      const { fields } = data;
+      const { studentCount } = data;
+
+      res.write('This is the list of our students\n');
+      res.write(`Number of students: ${studentCount}`);
+      Object.keys(fields).forEach((key) => {
+        if (key === '') return;
+        const value = fields[key];
+        res.write(`\nNumber of students in ${key}: ${value.length}. List: ${value.join(', ')}`);
+      });
+    })
+    .catch((err) => {
+      res.write(err.message);
+    })
+    .finally(() => {
+      res.end();
+    });
+};
+
+const app = http.createServer((req, res) => {
+  const endpoint = {
+    '/': index,
+    '/students': students,
+  };
+
+  endpoint[req.url](res);
+});
+
+app.listen(port, host);
+
+module.exports = app;
