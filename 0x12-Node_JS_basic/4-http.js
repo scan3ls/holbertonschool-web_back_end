74f52c959d3730757670b33fa915e3df8c0bca48
scan3ls <scan3ls@gmail.com>
2021-05-22 19:54:22 -0500

    Update

--- 0x12-Node_JS_basic/4-http.js
+++ 0x12-Node_JS_basic/4-http.js
@@ -5,7 +5,7 @@ const port = 1245;
 
 const app = http.createServer((req, res) => {
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text-plain');
+  res.setHeader('Content-Type', 'text/plain');
   res.end('Hello Holberton School!');
 });
 
