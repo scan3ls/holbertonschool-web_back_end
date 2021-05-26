0d1bc2000c68fa827e2980f8db1c547cfb2dd32a
scan3ls <scan3ls@gmail.com>
2021-05-26 13:47:16 -0500

    Update

---
+++ 0x13-unittests_in_js/4-payment.test.js
@@ -0,0 +1,19 @@
+const sendPaymentRequestToApi = require('./3-payment');
+const Utils = require('./utils');
+const chai = require('chai');
+const sinon = require('sinon');
+
+describe('Spying', () => {
+    it('Function called', () => {
+        const stub = sinon.stub(Utils, 'calculateNumber');
+        stub.returns(10);
+
+        const spy = sinon.spy(console, 'log');
+        const result = sendPaymentRequestToApi(100, 20);
+
+        chai.expect(stub.calledWith('SUM', 100, 20)).to.be.true;
+        chai.expect(spy.calledWith('The total is: 10')).to.be.true;
+
+        stub.restore();
+    });
+});
