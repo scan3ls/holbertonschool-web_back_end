da4f5a91b410ee4bb16cac204a9bd91f9e17b118
scan3ls <scan3ls@gmail.com>
2021-05-26 12:46:17 -0500

    update

--- 0x13-unittests_in_js/0-calcul.test.js
+++ 0x13-unittests_in_js/0-calcul.test.js
@@ -1,12 +1,30 @@
 const calculateNumber = require('./0-calcul');
 const assert = require('assert');
 
-it('First Number rounded', () => {
-    assert.equal(calculateNumber(0.7, 0), 1);
+describe('First Number rounded', () => {
+    it('Round up', () => {
+        assert.equal(calculateNumber(0.7, 0), 1);
+    });
+    it('Round Down', () => {
+        assert.equal(calculateNumber(0.2, 0), 0);
+    });
+
 });
-it('Second Number rounded', () => {
-    assert.equal(calculateNumber(0, 0.7), 1);
+
+describe('Second Number rounded', () => {
+    it('Round up', () => {
+        assert.equal(calculateNumber(0, 0.7), 1);
+    });
+    it('Round down', () =>{
+        assert.equal(calculateNumber(0, 0.2), 0);
+    });
 });
-it('Both Numbers Rounded', () =>{
-    assert.equal(calculateNumber(0.5, 0.5), 2);
+
+describe('Both Numbers Rounded', () => {
+    it('Rounded UP', () =>{
+        assert.equal(calculateNumber(0.5, 0.5), 2);
+    });
+    it('Rounded Down', () => {
+        assert.equal(calculateNumber(0.2, 0.2), 0);
+    });
 });
