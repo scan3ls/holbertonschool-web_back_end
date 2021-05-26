fa2859e821466b9e85f7778bd6268dbe69eefb61
scan3ls <scan3ls@gmail.com>
2021-05-26 12:54:18 -0500

    Update

--- 0x13-unittests_in_js/1-calcul.test.js
+++ 0x13-unittests_in_js/1-calcul.test.js
@@ -1,8 +1,17 @@
 const calculateNumber = require('./1-calcul');
 const assert = require('assert');
 
-describe('Calcultor functions', () => {
-    it('Addition', () => { assert.equal(calculateNumber('SUM', 1.4, 4.5), 6) });
-    it('Subraction', () => { assert.equal(calculateNumber('SUBTRACT', 1.4, 4.5), -4) });
-    it('Division', () => { assert.equal(calculateNumber('DIVIDE', 1.4, 0), 'Error') });
+
+
+describe('DIVISION', () => {
+    it('Divide by Zero', () => { assert.equal(calculateNumber('DIVIDE', 1.4, 0), 'Error') });
+    it('Valid Division', () => { assert.equal(calculateNumber('DIVIDE', 4, 2), 2) });
+});
+
+describe('ADDITION', () => {
+    it('Valid Addition', () => { assert.equal(calculateNumber('SUM', 1.4, 4.5), 6) });
+});
+
+describe('SUBTRACTION', () => {
+    it('Valid subtraction', () => { assert.equal(calculateNumber('SUBTRACT', 1.4, 4.5), -4) });
 });
