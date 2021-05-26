1e1de30ed5de9452016bc3faec4df7c081110d15
scan3ls <scan3ls@gmail.com>
2021-05-26 12:39:28 -0500

    Update

--- 0x13-unittests_in_js/0-calcul.test.js
+++ 0x13-unittests_in_js/0-calcul.test.js
@@ -1,9 +1,12 @@
 const calculateNumber = require('./0-calcul');
 const assert = require('assert');
 
-it('numbers containing fractions over 0.5 should round up', () => {
-    assert.equal(calculateNumber(0.4, 0.4), 1);
+it('First Number rounded', () => {
+    assert.equal(calculateNumber(0.7, 0), 1);
 });
-it('numbers containing fractions under 0.5 should round down', () => {
-    assert.equal(calculateNumber(0.2, 0.2), 0);
+it('Second Number rounded', () => {
+    assert.equal(calculateNumber(0, 0.7), 1);
+});
+it('Both Numbers Rounded', () =>{
+    assert.equal(calculateNumber(0.5, 0.5), 2);
 });
