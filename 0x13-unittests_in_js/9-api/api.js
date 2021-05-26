2698220f8e7748d8b4592f4ca2b54ab6187b9f3b
scan3ls <scan3ls@gmail.com>
2021-05-26 15:55:46 -0500

    Update

---
+++ 0x13-unittests_in_js/9-api/api.js
@@ -0,0 +1,25 @@
+const express = require('express');
+
+const app = express();
+const port = 7865;
+
+app.get('/', (req, res) => {
+  res.status(200);
+  res.send('Welcome to the payment system');
+});
+
+app.get('/cart/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+  if (isNaN(id)) {
+    res.status(404);
+    res.send('Id must be a number');
+  }
+  res.status(200);
+  res.send(`Payment methods for cart ${id}`);
+});
+
+app.listen(port, () => {
+  console.log(`API available on localhost port ${port}`);
+});
+
+module.exports = app;
