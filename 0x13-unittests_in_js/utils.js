9274aeeaed054a2946f6601e1e99bcc7728e6654
scan3ls <scan3ls@gmail.com>
2021-05-26 13:06:06 -0500

    Update

--- 0x13-unittests_in_js/utils.js
+++ 0x13-unittests_in_js/utils.js
@@ -2,7 +2,7 @@ const Utils = {
     calculateNumber(type, a, b) {
         switch (type) {
             case 'SUM':
-                return Math.round(a + b);
+                return Math.round(a) + Math.round(b);
             case 'SUBTRACT':
                 return Math.round(a) - Math.round(b);
             case 'DIVIDE':
